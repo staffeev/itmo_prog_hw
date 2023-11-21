5f01018065a5ef1001eeaf482e79dbc78215ad14
staffeev <st.affeev@yandex.ru>
2023-11-21 16:11:07 +0300

    purchase model

--- core/models/purchase.py
+++ core/models/purchase.py
@@ -1,11 +1,21 @@
 from sqlalchemy import Column, Integer, ForeignKey, String, DateTime
 from sqlalchemy.orm import relation
 from .db_session import SqlAlchemyBase
+import datetime
 
 
 class Purchase(SqlAlchemyBase):
     """Класс для ORM-модели покупки"""
     __tablename__ = "purchase"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    date = Column(DateTime)
+    date = Column(DateTime, default=datetime.datetime.now)
+    product_id = Column(Integer, ForeignKey("product.id"), nullable=False)
+    product = relation("Product", uselist=False)
     # product = relation("Product", secondary="purchase_to_product", back_populates="purchase")
+
+
+    def __repr__(self):
+        return f"Purchase(date={self.date.strftime('%d-%m-%Y')}, product_id={self.product_id})"
+    
+    def __str__(self):
+        return repr(self)
--- main.py
+++ main.py
@@ -2,14 +2,31 @@ from core.models import db_session
 from core.models.category import Category
 from core.models.product import Product
 from core.models.purchase import Purchase
+from datetime import datetime
 
 
 
 if __name__ == "__main__":
     db_session.global_init("puchaces.db")
     session = db_session.create_session()
-    p = session.query(Product).filter(Product.name == "prod1").first()
-    print(p.categories)
+    pc = session.query(Purchase).first()
+    print(pc.date, pc.product_id, pc.product)
+    # p = session.query(Product).filter(Product.id == 1).first()
+    # c = Category(name="cat1")
+    # p = Product(name="prod1", cost=200)
+    # p.categories.append(c)
+    # session.add(c)
+    # session.add(p)
+
+    # pc = Purchase(product_id=p.id)
+    # pc.product = p
+    # session.add(pc)
+    # print(pc.date)
+    # session.add(pc)
+    # p = session.query(Product).filter(Product.name == "prod1").first()
+    # c = session.query(Category).filter(Category.name == "test1").first()
+    # session.delete(c)
+    # print(p.categories)
     # c = Category(name="test1")
     # c2 = Category(name="test2")
     # c3 = Category(name="test3")
